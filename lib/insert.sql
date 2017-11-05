16a9e9580dbb7ee55786cb85a8a5cfcc95535084
George Gore <georgelgore@gmail.com>
2017-11-05 15:04:08 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'sweet', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'F', 'black', 'mean', 0);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 21, 'F', 'red', 'nice', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'M', 'black', 'mean', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 1, 'F', 'red', 'nice', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 33, 'M', 'blue', 'ok', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 1, 'F', 'red', 'rude', 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 21, 'F', 'red', 'nice', 1);
+
+-- name TEXT,
+-- age INTEGER,
+-- gender CHAR(1),
+-- color TEXT,
+-- temperament TEXT,
+-- alive BOOLEAN
